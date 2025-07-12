9550a2e8f728f39bc18d871076c8134a6f7fb329
Akash <akashkewat99@gmail.com>
2025-07-12 17:18:30 +0530

    fixed footer name convention error

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,6 +1,9 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./Header/Header";
-import { Footer } from "./Footer/FOoter";
+import {Footer} from "./Footer/Footer"
+
+
+
 
 export const AppLayout = () => {
   return (
